6e3af7668dea5a3473d6f115cee1b950ee497809
Hackathon <l.eom1994@hotmail.com>
2020-06-14 23:50:45 -0300

    Projeto web

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// JSX Sintaxe de XML dentro do JavaScript
+import Rotas from './rotas';
+
+function App() {
+  return (
+    <Rotas />
+  );
+}
+
+export default App;
