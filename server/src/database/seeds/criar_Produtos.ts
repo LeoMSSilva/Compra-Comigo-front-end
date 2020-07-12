d1abc12d813e4d0f02a99b0f918b5a0dbc5d0326
LeoMSSilva <l.eom1994@hotmail.com>
2020-07-11 21:50:37 -0300

    Só deixei o front-end

--- server/src/database/seeds/criar_Produtos.ts
+++
@@ -1,9 +0,0 @@
-import Knex from 'knex';
-
-export async function seed(knex: Knex) {
-    await knex('Produtos').insert([
-        { nome: 'Maizena', marca: 'Piraque', modelo: 'n/a', preço: '3.69', idCategoriasDeProduto:'1', idSubCategoriasDeProduto: '1'},
-        { nome: 'Maria', marca: 'Richester', modelo: 'n/a', preço: '5.30', idCategoriasDeProduto:'1', idSubCategoriasDeProduto: '1'},
-        { nome: 'Leite', marca: 'Elegê', modelo: 'n/a', preço: '3.49', idCategoriasDeProduto:'1', idSubCategoriasDeProduto: '1'},
-    ])
-}
